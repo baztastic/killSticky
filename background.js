a94fa0f3912b2971924493e0bee3cc193ae03a7d
baztastic <pkunzip@gmail.com>
2018-09-07 09:51:58 +0100

    removed extra killSticky.js file

--- background.js
+++ background.js
@@ -1,10 +1,20 @@
 // Kill sticky headers
-// opens the js file killSticky.js when the button is clicked
-// Original bookmarklet:
+// runs `code` when the button is clicked
+// Original bookmarklet by Alisdair McDiarmid:
 // https://alisdair.mcdiarmid.org/kill-sticky-headers/
 
 'use strict';
 
+const code = `(() => {
+  var i, elements = document.querySelectorAll('body *');
+
+    for (i = 0; i < elements.length; i++) {
+      if (["sticky", "fixed"].includes(getComputedStyle(elements[i]).position)) {
+        elements[i].parentNode.removeChild(elements[i]);
+      }
+    }
+})()`
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "killSticky.js"});
+   chrome.tabs.executeScript({ code });
 });
